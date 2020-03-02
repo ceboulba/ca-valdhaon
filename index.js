bf1178487d9ce45cdfb52cd59ae6df068195b523
ceboulba <antoine@archipicture.fr>
2020-03-02 09:26:37 +0100

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1583108857/ca-valdahon/cafc-Valdhon-02_View03.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1583108853/ca-valdahon/cafc-Valdhon-02_View04.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1583108856/ca-valdahon/cafc-Valdhon-02_View05.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583112552/ca-valdahon/cafc-Valdhon-02_View06.jpg',
+  'https://res.cloudinary.com/archipicture/image/upload/v1583137581/ca-valdahon/cafc-Valdhon-02_View06.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1583135890/ca-valdahon/cafc-Valdhon-02_View07.jpg',
 ]
 const imgName= [
